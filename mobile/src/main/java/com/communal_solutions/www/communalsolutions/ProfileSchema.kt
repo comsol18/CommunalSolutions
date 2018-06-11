d1d2b2363bfd1858b0241f00eb59dbce9e3d536e
John Parry <ogparry1@ufl.edu>
2018-06-11 13:14:54 -0400

    can write to profile, reading not ready yet

--- mobile/src/main/java/com/communal_solutions/www/communalsolutions/ProfileSchema.kt
+++ mobile/src/main/java/com/communal_solutions/www/communalsolutions/ProfileSchema.kt
@@ -1,41 +1,27 @@
 package com.communal_solutions.www.communalsolutions
 
-class ProfileSchema(username: String, profileName: String, phoneNum: String, status: String){
+import com.google.firebase.auth.FirebaseAuth
 
-    private var dbHandler: DatabaseHandler = DatabaseHandler()
+class ProfileSchema(profileName: String, phoneNum: String, status: String){
 
     // profile info
-    private var uid = -1
-    private var username = ""
-    private var profileName = ""
-    private var phoneNum = ""
-    private var status = ""
+    var profileName = ""
+    var username = ""
+    var phoneNum = ""
+    var eMail = ""
+    var status = ""
+
+    /* list of emergency contacts and location
+    private var contacts = arrayOfNulls<String>(6)
+    private var gpsCoords = arrayOfNulls<String>(2)
+    */
 
     init {
-        this.username = username
+        this.eMail = FirebaseAuth.getInstance().currentUser!!.email.toString()
+        this.username = this.eMail.substringBefore('@', "")
         this.profileName = profileName
         this.phoneNum = phoneNum
         this.status = status
-    }
-
-    // list of emergency contacts and location
-    private var contacts = arrayOfNulls<String>(6)
-    private var gpsCoords = arrayOfNulls<String>(2)
-
-    // Get Data
-    fun getUsername(): String { return username }
-    fun getProfileName(): String { return profileName }
-    fun getPhoneNum(): String { return phoneNum }
-    fun getStatus(): String { return status }
-    fun getContact(which: Int): String { return if (contacts[which] != null) contacts[which]!! else "null" }
-
-    // Set Data
-    fun updateUsername(newUsername: String) { username = newUsername }
-    fun updateProfileName(newProfileName: String) { profileName = newProfileName }
-    fun updatePhoneNum(newPhoneNumber: String) { phoneNum = newPhoneNumber }
-    fun updateStatus(newStatus: String) { status = newStatus }
-    fun updateContact(which: Int, contactName: String) {
-        contacts[which] = contactName
     }
 
 }
--- mobile/src/main/java/com/communal_solutions/www/communalsolutions/SettingsActivity.kt
+++ mobile/src/main/java/com/communal_solutions/www/communalsolutions/SettingsActivity.kt
@@ -2,33 +2,42 @@ package com.communal_solutions.www.communalsolutions
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import kotlinx.android.synthetic.main.activity_settings.*
+
 
 class SettingsActivity : AppCompatActivity() {
 
-    private var dbHandler: DatabaseHandler = DatabaseHandler()
+    private var db = FirebaseDatabase.getInstance()
+    private var cUser = FirebaseAuth.getInstance().currentUser!!
+    private var profileSchema = ProfileSchema("","----------","fine")
 
-    private fun initValues() {
-        val name = findViewById<EditText>(R.id.editDisplayName)
-        val email = findViewById<EditText>(R.id.editEmail)
-        val num = findViewById<EditText>(R.id.editPhoneNum)
-        val uName = findViewById<EditText>(R.id.editUsername)
+    fun updateProfile(view: View) {
+        val users = db.getReference("users")
 
-        name.setText(dbHandler.getDisplayName())
-        email.setText(dbHandler.getEmail())
-        num.setText(dbHandler.getPhoneNumber())
-        uName.setText(dbHandler.getUserName())
+        val editStatus = findViewById<EditText>(R.id.editStatus)
+        val editName = findViewById<EditText>(R.id.editDisplayName)
+        val editNum = findViewById<EditText>(R.id.editPhoneNum)
+
+        profileSchema.profileName = editName.text.toString()
+        profileSchema.phoneNum = editNum.text.toString()
+        profileSchema.status = editStatus.text.toString()
+
+        users.child(cUser.uid.hashCode().toString()).setValue(profileSchema)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        initValues()
-
-        //val test: EditText = findViewById(R.id.testField)
-        //test.setOnEditorActionListener( TextView.OnEditorActionListener())
+        
     }
 }
