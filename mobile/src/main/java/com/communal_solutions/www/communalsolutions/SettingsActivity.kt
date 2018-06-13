71ab21b7987f089c0ef2e8d3dd612e8e72b230c6
John Parry <ogparry1@ufl.edu>
2018-06-13 00:27:47 -0400

    added location manager and location to database schema

--- mobile/src/main/java/com/communal_solutions/www/communalsolutions/SettingsActivity.kt
+++ mobile/src/main/java/com/communal_solutions/www/communalsolutions/SettingsActivity.kt
@@ -1,7 +1,14 @@
 package com.communal_solutions.www.communalsolutions
 
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.util.JsonToken
 import android.util.Log
 import android.view.View
@@ -12,6 +19,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_settings.*
+import kotlin.math.roundToInt
 
 data class Profile(
     // profile info
@@ -20,6 +28,12 @@ data class Profile(
     val cell_number: String = "",
     val email_address: String = "",
     val status: String = "",
+    var uuid: String = ""
+)
+
+data class UserLocation(
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
     var uuid: String = ""
 )
 
@@ -32,6 +46,8 @@ class SettingsActivity : AppCompatActivity() {
     private var userReference: DatabaseReference? = null
     private var profileListener: ValueEventListener? = null
     private var uid: String? = null
+
+    private var locationManager: LocationManager? = null
 
     private fun writeProfileData(profile: Profile) {
         val uid = cUser!!.uid.hashCode().toString()
@@ -51,12 +67,6 @@ class SettingsActivity : AppCompatActivity() {
         // push data to database
         val updateListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val userData = dataSnapshot.getValue(Profile::class.java)
-                /*if (userData == null) {
-                    Log.e("Error", "onDataChange: User data is null!")
-                    Toast.makeText(this@SettingsActivity, "onDataChange: User data is null!", Toast.LENGTH_SHORT).show()
-                    return
-                }*/
                 writeProfileData(profile)
             }
 
@@ -66,6 +76,21 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         dbReference!!.child("users").child(uid!!).addListenerForSingleValueEvent(updateListener)
+    }
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            val lat: Double = Math.round(location.latitude*1000.0)/1000.0
+            val long: Double = Math.round(location.longitude*1000.0)/1000.0
+            gpsCoordinates.setText("(${lat}, ${long})")
+
+            val userLocation = UserLocation(location.latitude, location.longitude, uid!!)
+            val locReference = db!!.getReference("locations")
+            locReference.child(uid!!).setValue(userLocation)
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -82,6 +107,26 @@ class SettingsActivity : AppCompatActivity() {
         // set onclicklistener for save button
         saveSettings.setOnClickListener {
             updateProfile()
+        }
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+        val MY_PERMISSIONS_REQUEST_LOCATION = 9003
+
+        try {
+            // Request location updates
+            if (ContextCompat.checkSelfPermission(this@SettingsActivity, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(this@SettingsActivity,
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            MY_PERMISSIONS_REQUEST_LOCATION)
+            } else {
+                // Permission has already been granted
+                locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+            }
+        } catch(ex: SecurityException) {
+            Log.e("Exception", ex.toString())
+            Log.e("myTag", "Security Exception, no location available");
         }
     }
 
@@ -110,10 +155,15 @@ class SettingsActivity : AppCompatActivity() {
                 Toast.makeText(this@SettingsActivity, "Error: Failed to read user data", Toast.LENGTH_LONG).show()
             }
         }
-        //add listener to reference
+
+        // add listener to reference
         userReference!!.addValueEventListener(profileListener)
-        //set global listener to the listener defined
+
+        // set global listener to the listener defined
         this.profileListener = profileListener
+
+        // get GPS coordinates and update location in db
+
     }
 
     override fun onStop() {
